17e69104e9e3d94b894171e93e9f3da8cc2d64e9
ksafin <ksafin@gmail.com>
2017-12-12 10:13:16 +0000

    Latest test file

--- PythonProject/test.py
+++ PythonProject/test.py
@@ -6,25 +6,9 @@ worked = test.begin()
 print worked
 
 motor1 = test.getmotor(1)
-motor1.setPWM(100)
-motor1.setPPR(motor1.NEVEREST_60_PPR);
-time.sleep(1);
-spd = motor1.getSpeed()
-curr = motor1.getCurrent()
-enc = motor1.getEncoder()
-print enc
-print curr
-print spd
-motor1.resetEncoder()
-enc = motor1.getEncoder()
-print enc
-motor1.enableCoast()
-motor1.enableBrake()
-motor1.setCoastSpeed(1000)
-#motor1.release()
-fault = motor1.getFault()
-print fault
-motor1.disablePID()
-motor1.stop()
-#motor1.setCurrentLimit(10000);
-#motor1.setPID(0.8, 0.2, 0.05)
+
+motor1.setPWM(0);
+
+while True:
+    print motor1.getEncoder()
+    time.sleep(0.001)
